6660e659e6b91e601d8bf732f2ba921f8e40721c
Sumit Kumar <ursumit@gmail.com>
2019-06-19 15:03:42 +0530

    Added sample scripts

---
+++ mqtt-tools/brokerAutoRestart.sh
@@ -0,0 +1,17 @@
+#*/5 * * * * /home/ubuntu/mosquitto_restart.sh
+
+if [ "`ps -aux | grep /usr/sbin/mosquitto | wc -l`" == "1" ]
+
+then
+
+        echo "mosquitto wasnt running so attempting restart" >> /home/ubuntu/cron.log
+
+        systemctl restart mosquitto
+
+        exit 0
+
+fi
+
+echo "$SERVICE is currently running" >> /home/ubuntu/cron.log
+
+exit 0
