c0cd6ee7ad676e4abe0ff0a97a5d50f5ab2bf3ac
Yogesh <mr.yogesh@gmail.com>
2019-07-03 08:26:56 +0530

    Add files via upload
    Added source code to perform direct OBD data scan capture and transfer to local FS.

---
+++ connectedCars/onlyObd1.py
@@ -0,0 +1,146 @@
+'''
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES 
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+ '''
+
+import logging
+import time
+import argparse
+import json
+import random
+import obd
+import time
+import timeit
+
+# We enable all logging messages for initial development
+obd.logger.setLevel(obd.logging.DEBUG)
+#obd.logger.setLevel(obd.logging.INFO)
+
+print ("Establishing MQTT Connection")
+#Get a list of all Serial ports available
+print(obd.scan_serial())
+
+
+print("Initilizing OBD connection...")
+connection = obd.OBD()
+
+print("OBD connection established.")
+# List of commands
+commands_list = ['ABSOLUTE_LOAD', 'ACCELERATOR_POS_D', 'ACCELERATOR_POS_E', 'ACCELERATOR_POS_F', 'AIR_STATUS', 'AMBIANT_AIR_TEMP', 'AUX_INPUT_STATUS', 'BAROMETRIC_PRESSURE', 'CATALYST_TEMP_B1S1', 'CATALYST_TEMP_B1S2', 'CATALYST_TEMP_B2S1', 'CATALYST_TEMP_B2S2', 'COMMANDED_EGR', 'COMMANDED_EQUIV_RATIO', 'CONTROL_MODULE_VOLTAGE', 'COOLANT_TEMP', 'DISTANCE_SINCE_DTC_CLEAR', 'DISTANCE_W_MIL', 'EGR_ERROR', 'ELM_VERSION', 'ELM_VOLTAGE', 'EMISSION_REQ', 'ENGINE_LOAD', 'ETHANOL_PERCENT', 'EVAPORATIVE_PURGE', 'EVAP_VAPOR_PRESSURE', 'EVAP_VAPOR_PRESSURE_ABS', 'EVAP_VAPOR_PRESSURE_ALT', 'FREEZE_DTC', 'FUEL_INJECT_TIMING', 'FUEL_LEVEL', 'FUEL_PRESSURE', 'FUEL_RAIL_PRESSURE_ABS', 'FUEL_RAIL_PRESSURE_DIRECT', 'FUEL_RAIL_PRESSURE_VAC', 'FUEL_RATE', 'FUEL_STATUS', 'FUEL_TYPE', 'GET_CURRENT_DTC', 'GET_DTC', 'HYBRID_BATTERY_REMAINING', 'INTAKE_PRESSURE', 'INTAKE_TEMP', 'LONG_FUEL_TRIM_1', 'LONG_FUEL_TRIM_2', 'LONG_O2_TRIM_B1', 'LONG_O2_TRIM_B2', 'MAF', 'MAX_MAF', 'MAX_VALUES', 'O2_B1S1', 'O2_B1S2', 'O2_B1S3', 'O2_B1S4', 'O2_B2S1', 'O2_B2S2', 'O2_B2S3', 'O2_B2S4', 'O2_S1_WR_CURRENT', 'O2_S1_WR_VOLTAGE', 'O2_S2_WR_CURRENT', 'O2_S2_WR_VOLTAGE', 'O2_S3_WR_CURRENT', 'O2_S3_WR_VOLTAGE', 'O2_S4_WR_CURRENT', 'O2_S4_WR_VOLTAGE', 'O2_S5_WR_CURRENT', 'O2_S5_WR_VOLTAGE', 'O2_S6_WR_CURRENT', 'O2_S6_WR_VOLTAGE', 'O2_S7_WR_CURRENT', 'O2_S7_WR_VOLTAGE', 'O2_S8_WR_CURRENT', 'O2_S8_WR_VOLTAGE', 'O2_SENSORS', 'O2_SENSORS_ALT', 'OBD_COMPLIANCE', 'OIL_TEMP', 'PIDS_A', 'PIDS_B', 'PIDS_C', 'RELATIVE_ACCEL_POS', 'RELATIVE_THROTTLE_POS', 'RPM', 'RUN_TIME', 'RUN_TIME_MIL', 'SHORT_FUEL_TRIM_1', 'SHORT_FUEL_TRIM_2', 'SHORT_O2_TRIM_B1', 'SHORT_O2_TRIM_B2', 'SPEED', 'STATUS', 'STATUS_DRIVE_CYCLE', 'THROTTLE_ACTUATOR', 'THROTTLE_POS', 'THROTTLE_POS_B', 'THROTTLE_POS_C', 'TIME_SINCE_DTC_CLEARED', 'TIMING_ADVANCE', 'WARMUPS_SINCE_DTC_CLEAR']
+
+# wait for the connection to be established and car ignition to be turned on
+while not connection.status() == obd.OBDStatus.CAR_CONNECTED:
+    print("Waiting for ignition to start. Check will be performed again in 1 second")
+    print(connection.status())
+    time.sleep(2)
+
+# First we check and create a list of supported sub set of commands for  the connected vehicle
+supported_commands_list = []
+for i in commands_list:
+    c = obd.commands[i]
+    if connection.supports(c):
+        supported_commands_list.append(i)
+    else:
+        continue
+
+messageJson_array = []
+count = 0
+
+while True:
+        start_10=0
+        end_10=0
+
+        # Query all commands in serial fashion
+        # Note this can be changed to asynchronous format if required
+        for i in supported_commands_list:
+            if start_10==0:
+                start_10 = timeit.timeit()
+            start = timeit.timeit()
+            c = obd.commands[i]
+            response = connection.query(c)
+            #if not response.is_null():
+            #    print(i + ": " + str(response.value) + ' ***EOV \n\n')
+            x ={}
+            '''
+            for i in response.messages:
+                
+                print('frames')
+                print(dir(i.frames))
+                print('hex')
+                print(dir(i.hex))
+                print('parsed')
+                print(dir(i.parsed))
+                print('raw')
+                print(dir(i.raw))
+                print('tx_id')
+                print(dir(i.tx_id))
+                #x.append([i.data, i.ecu, i.frames, i.hex, i.parsed, i.raw, i.tx_id])
+                x['data'] = i.data
+                x['ecu'] = i.ecu
+                x['frames'] = str(i.frames[:])
+                x['hex'] = i.hex.__str__
+                x['parsed'] = i.parsed.__str__
+                x['raw'] = i.raw.__str__
+                if i.tx_id is not None:
+                    x['tx_id'] = str(i.tx_id.real) + ' ' + str(i.tx_id.imag)
+            '''
+
+
+            python_object = {
+                'Device_ID':'RaspbereyPi_bf4e',
+                'time':str(response.time),
+                'Parameter':str(i),
+                'Command':str(response.command),
+                #'RawMessage':str(response.messages),
+                #'RawMessage':str(x),
+                'Value:':str(response.value)
+                }
+            messageJson_array.append(python_object)
+            count +=1
+            print(str(count) + " of " + str(10*len(supported_commands_list)) + " messages held in buffer.")
+            end = timeit.timeit()
+            #print(str(end - start) + " is time required for " + str(i))
+
+            #if count>=10*len(supported_commands_list) and end_10==0:
+            #    print("*"*80)
+            #    print("*"*80)
+            #    print("*"*80)
+            #    print("*"*80)
+            #    end_10=timeit.timeit()
+            #    print("Time for 10 set of records = " + str(end_10-start_10))
+            #    start_10=0
+            #    end_10=0
+                
+
+            # If number of messages collected becomes 100, we wirite them all to a file
+            #if count>=10*len(supported_commands_list):
+            if True:
+                with open('/home/pi/aws_sdk/aws-iot-device-sdk-python/samples/basicPubSub/Certs/result.json', 'a+') as f:
+                    #print("*"*80)
+                    #print("*"*80)
+                    #print("*"*80)
+                    #start_file = timeit.timeit()
+                    #print("Time between end of first write and beginning of next was " + str(start_file-end_file))
+                    #print(messageJson_array)
+                    for item in messageJson_array:
+                        f.write("%s\n" % item)
+                    end_file = timeit.timeit()
+                    print("Written to file")
+                    #print("Time to write " + str(count) + " record was " + str(end_file-start_file))
+                    messageJson_array = []
+                    count = 0
+                #print("*"*80)
+                #print("*"*80)
+                #print("*"*80)
+            #myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+            #if args.mode == 'publish':
+            #    print('Published topic %s: %s\n' % (topic, messageJson))
+            #print('Published topic %s: %s\n' % (topic, messageJson))
+    #time.sleep(0.1)
